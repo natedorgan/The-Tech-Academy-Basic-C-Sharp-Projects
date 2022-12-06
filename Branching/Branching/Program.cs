d2bf23aa5c2da50db3af21857ea989f8c842de25
natedorgan <natedorgandevops@gmail.com>
2022-12-05 18:47:56 -0800

    Add files via upload

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the package's weight?");
+            string packInput = Console.ReadLine();
+            int packWeight = Convert.ToInt32(packInput);
+            if(packWeight>50)
+                {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                }
+            else
+                {
+                Console.WriteLine("What is the package's width?");
+                string widthInput = Console.ReadLine();
+                int packWidth = Convert.ToInt32(widthInput);
+                Console.WriteLine("What is the package's height?");
+                string heightInput = Console.ReadLine();
+                int packHeight = Convert.ToInt32(heightInput);
+                Console.WriteLine("What is the package's length?");
+                string lengthInput = Console.ReadLine();
+                int packLength = Convert.ToInt32(lengthInput);
+                int overallDimensions = (packLength * packHeight * packWidth);
+                int dimTotal = overallDimensions * packWeight;
+                decimal dimDec = Convert.ToDecimal(dimTotal);                //convert to decimal for cent values
+                decimal shipCost = dimDec / 100;
+                string endResult = overallDimensions > 50 ? "Package too big to be shipped via Package Express." : "Your estimated total for this package is: $" + shipCost;
+                Console.WriteLine(endResult);
+                Console.ReadLine();
+                }
+        }
+    }
+}

7fbd8288ef5dabffba482a698552a53356a7fa2a
natedorgan <natedorgandevops@gmail.com>
2022-12-05 17:20:53 -0800

    Update Program.cs

--- Math_Comparison/Math_Comparison/Program.cs
+++ Math_Comparison/Math_Comparison/Program.cs
@@ -15,7 +15,7 @@ namespace Math_Comparison
             Console.WriteLine("How many hours do you work per week?");
             string p1hoursWorked = Console.ReadLine();
             int p1hourWork = Convert.ToInt32(p1hoursWorked);
-            int p1salTotal = p1hourWork * p1hourRate;
+            int p1salTotal = p1hourWork * p1hourRate * 52;
             Console.WriteLine("Person 2");
             Console.WriteLine("What is your Hourly Rate?");
             string p2hourlyRate = Console.ReadLine();
@@ -23,7 +23,7 @@ namespace Math_Comparison
             Console.WriteLine("How many hours do you work per week?");
             string p2hoursWorked = Console.ReadLine();
             int p2hourWork = Convert.ToInt32(p2hoursWorked);
-            int p2salTotal = p2hourWork * p2hourRate;
+            int p2salTotal = p2hourWork * p2hourRate * 52;
             Console.WriteLine("Annual salary of Person 1:" + p1salTotal);
             Console.WriteLine("Annual salary of Person 2:" + p2salTotal);
             bool greaterThan = p1salTotal > p2salTotal;

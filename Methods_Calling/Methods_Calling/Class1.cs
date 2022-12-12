bcfa294d0e0bad40ae2ff060d3b721a3c3424512
natedorgan <natedorgandevops@gmail.com>
2022-12-12 11:03:32 -0800

    Add files via upload

---
+++ Methods_Calling/Methods_Calling/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Calling
+{
+    public class Class1
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+        public int Subtract(int data, int value)
+        {
+            return data - value;
+        }
+        public int Divide(int data, int value)
+        {
+            return data / value;
+        }
+    }
+}
---
+++ Methods_Calling/Methods_Calling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Calling
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter input:");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+
+            var class1Object = new Class1();
+            var result = class1Object.Add(userInput, 4);
+            result = class1Object.Subtract(result, 2);
+            result = class1Object.Divide(result, 1);
+
+            Console.WriteLine(userInput + " + 4 " + " - 2 " + " / 1 = " + result);
+            Console.ReadLine();
+        }
+    }
+}

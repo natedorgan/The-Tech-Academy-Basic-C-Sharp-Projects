9eddc330e41f653cf068a39ad6de0ae5a809b58d
natedorgan <natedorgandevops@gmail.com>
2022-12-12 08:40:45 -0800

    Add files via upload

---
+++ Try_Catch/Try_Catch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                List<int> intList = new List<int>() { 10, 20, 30, 40, 50 };
+                Console.WriteLine("Pick a number to divide the list by");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in intList)
+                {
+                    Console.WriteLine(number + " divided by " + userInput + " is: ");
+                    Console.WriteLine(number / userInput);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("Continuing Loop");
+        }
+    }
+}

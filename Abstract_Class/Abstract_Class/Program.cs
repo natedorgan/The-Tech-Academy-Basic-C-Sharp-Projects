f6b08ce6bfe4c85b9f27b4a4287b6976217889f8
natedorgan <natedorgandevops@gmail.com>
2022-12-13 07:44:05 -0800

    Add files via upload

---
+++ Abstract_Class/Abstract_Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee sample = new Employee();
+            sample.FirstName = "Sample";
+            sample.LastName = "Student";
+            sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}

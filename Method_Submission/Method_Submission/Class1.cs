dde8baf07027ad103f6c1329fcd3d8951096c085
natedorgan <natedorgandevops@gmail.com>
2022-12-12 16:27:47 -0800

    Add files via upload

---
+++ Method_Submission/Method_Submission/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission
+{
+    public class Class1
+    {
+        public int Method(int num1, int num2=1)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Method_Submission/Method_Submission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Method_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number:");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Would you like to enter a second number? Answer yes or no");
+            var Continue = Console.ReadLine();
+            if (Continue == "yes")
+            {
+                Console.WriteLine("Enter a number:");
+                var inputTwo = Convert.ToInt32(Console.ReadLine());
+                var mathObject = new Class1();
+                var result = mathObject.Method(userInput, inputTwo);
+                Console.WriteLine(userInput + " + " + inputTwo + " = " + result);
+                Console.ReadLine();
+            }
+            if (Continue == "no")
+            {
+                var mathObject2 = new Class1();
+                var result2 = mathObject2.Method(userInput);
+                Console.WriteLine(userInput + " + 1 = " + result2);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}

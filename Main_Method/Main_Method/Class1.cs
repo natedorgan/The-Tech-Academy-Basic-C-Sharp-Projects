6ac7006827686cd9949370140aaf7f640abfe94f
natedorgan <natedorgandevops@gmail.com>
2022-12-12 16:02:39 -0800

    Add files via upload

---
+++ Main_Method/Main_Method/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    public class Math
+    {
+        public int Operator(int data)
+        {
+            return data + 2;
+        }
+        public int Operator(decimal number)
+        {
+            int intNumber = Convert.ToInt32(number);
+            return intNumber * 2;
+        }
+        public int Operator(string String)
+        {
+            int intString = Convert.ToInt32(String);
+            return intString / 2;
+        }
+    }
+}
---
+++ Main_Method/Main_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main()
+        {
+            var mathObject = new Math();
+            var result = mathObject.Operator(4);
+            var mathObject2 = new Math();
+            var result2 = mathObject2.Operator(Convert.ToDecimal(4.4));
+            var mathObject3 = new Math();
+            var result3 = mathObject3.Operator("4");
+
+
+            Console.WriteLine("4 + 2 = " + result);
+            Console.WriteLine("4.4 * 2  = " + result2);
+            Console.WriteLine("4 / 2 = " + result3);
+            Console.ReadLine();
+        }
+    }
+}

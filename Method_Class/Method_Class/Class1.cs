f140a8ca19b06306e0c666717ebc8f65e4cb83eb
natedorgan <natedorgandevops@gmail.com>
2022-12-12 16:38:59 -0800

    Add files via upload

---
+++ Method_Class/Method_Class/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Class1
+    {
+        public void Method(int int1, int int2)
+        {
+            var result = int1 + 1;
+            Console.WriteLine(int2);
+        }
+    }
+}
---
+++ Method_Class/Method_Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathObject = new Class1();
+            mathObject.Method(1, 2);
+            Console.ReadLine();
+        }
+    }
+}

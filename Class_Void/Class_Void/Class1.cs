3f650af3d802f4962bc3fedb7852e8eb283dadee
natedorgan <natedorgandevops@gmail.com>
2022-12-12 18:02:03 -0800

    Add files via upload

---
+++ Class_Void/Class_Void/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_Void
+{
+    class Class1
+    {
+        public void Method(int num)
+        {
+            var result = num / 2;
+            Console.WriteLine(result);
+        }
+        public void Method(int num1, int num2)
+        {
+            var total = num1 + num2;
+        }
+        public void Method2(out int a)
+        {
+            a = 2;
+        }
+    }
+    static class Class2
+    {
+
+    }
+}
---
+++ Class_Void/Class_Void/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Void
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathObject = new Class1();
+            Console.WriteLine("Please enter a number:");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            mathObject.Method(userInput);
+            Console.ReadLine();
+        }
+    }
+}

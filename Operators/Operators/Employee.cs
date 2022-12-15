894f7d03808e26c4804d979c3c7c8503616ddcf8
natedorgan <natedorgandevops@gmail.com>
2022-12-15 09:36:36 -0800

    Add files via upload

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+ 
+        public static bool operator == (Employee employee1, Employee employee2)
+        {                  
+            {
+                return true;
+            }
+        }
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            return false;
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example1 = new Employee();
+            Employee example2 = new Employee();
+
+            example1.ID = 1;
+            example2.ID = 2;
+
+            if (example1 == example2)
+            {
+                Console.WriteLine(true);
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}

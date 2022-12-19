411c801a516d053d51bd13c975eaa67cd752234d
natedorgan <natedorgandevops@gmail.com>
2022-12-19 10:27:51 -0600

    Add files via upload

---
+++ Overloaded_Operators/Overloaded_Operators/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Operators
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID != employee2.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Overloaded_Operators/Overloaded_Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.ID = 1;
+            employee2.ID = 2;
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+            Console.ReadLine();
+        }
+    }
+}

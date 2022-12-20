33bdb9aef00e57e12e1db92c9fcce2ad9893a6ff
natedorgan <natedorgandevops@gmail.com>
2022-12-20 17:19:47 -0600

    Add files via upload

---
+++ Lambda_Submission/Lambda_Submission/EmpList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission
+{
+    public class EmpList
+    {
+        public List<Employee> EmpLists()
+        {
+            List<Employee> Emps = new List<Employee>();
+            List<string> Names = new List<string>() { "Joe", "Non-Joe" };
+            List<int> Ids = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };             
+
+            foreach (string name in Names)
+            {
+                foreach (int Id in Ids)
+                {
+                    Employee emp = new Employee();
+                    emp.FirstName = name;
+                    emp.ID = Id;
+                    Emps.Add(emp);                               
+                }
+            }
+            return Emps;
+        }
+        
+    }
+}
---
+++ Lambda_Submission/Lambda_Submission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission
+{
+    class Program                         
+    {
+        static void Main(string[] args)
+        {
+            EmpList Emps = new EmpList();
+
+            List<Employee> joeList = new List<Employee>();
+
+            foreach (Employee emp in Emps.EmpLists())
+            {
+                if (emp.FirstName == "Joe")
+                {
+                    joeList.Add(emp);
+                }
+            }
+            List<Employee> lambdaJoe = Emps.EmpLists().Where(x => x.FirstName == "Joe").ToList();
+            List<Employee> lambdaInt = Emps.EmpLists().Where(x => x.ID > 5).ToList();
+            Console.ReadLine();
+            
+        }   
+        
+
+    }
+    
+}

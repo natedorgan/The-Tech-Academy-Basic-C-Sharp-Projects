ad42c9d484000a203b4d2d9cac440a0193e37d21
natedorgan <natedorgandevops@gmail.com>
2022-12-19 23:11:30 -0600

    Add files via upload

---
+++ Lambda_Submission/Lambda_Submission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpLists();
+
+            List<Employee> joeList = new List<Employee>();
+
+            foreach (Employee emp in Emps)
+            {
+                if (emp.FirstName == "Joe")
+                {
+                    joeList.Add(emp);
+                }
+            }
+            List<Employee> lambdaJoe = new List<EmpList> Emps.Where(x => x.FirstName == "Joe").ToList();
+            List<Employee> lambdaInt = new List<EmpList> Emps.Where(x => x.ID > 5).ToList();
+
+        }   
+
+    }
+}

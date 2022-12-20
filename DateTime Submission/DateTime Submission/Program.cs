15b368de298b9971632178deb9afb7f09e4f7a2a
natedorgan <natedorgandevops@gmail.com>
2022-12-20 10:24:43 -0600

    Add files via upload

---
+++ DateTime Submission/DateTime Submission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            DateTime currentTime = DateTime.Now; 
+            Console.WriteLine("Please enter a number");
+            DateTime userNum = DateTime.ParseExact(Console.ReadLine(), "HH", null);
+            Console.WriteLine(userNum + currentTime);
+        }
+    }
+}

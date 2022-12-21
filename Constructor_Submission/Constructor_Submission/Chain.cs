0a1e81cf93d5c3eb6096b2610f6b6dd6b2852207
natedorgan <natedorgandevops@gmail.com>
2022-12-20 18:36:14 -0600

    Add files via upload

---
+++ Constructor_Submission/Constructor_Submission/Chain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Chain
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Chain(string name) : this(name, 1)
+        {
+        }
+        public Chain(string name, int id)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Constructor_Submission/Constructor_Submission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+        public const string Title = "Example";
+        static void Main(string[] args)
+        {
+            var arbitrary = 4;
+            int answer = arbitrary + 4;
+            Console.WriteLine(Title + " " + answer);
+            Chain newChain = new Chain("Test");
+            Console.WriteLine(newChain.Id);
+            Console.ReadLine();
+
+        }
+    }
+}
